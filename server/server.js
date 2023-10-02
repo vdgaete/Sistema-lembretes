8ab2afb1dedf026c45bdeedd49df7889d9c24405
vdgaete <vdgaete@gmail.com>
2023-10-02 09:20:06 -0300

    server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
 import path from "path";
 import express from "express";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import process from "process";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
 const app = express(); // create express app
 
 // add middlewares
 app.use(express.static(path.join("..", "dist")));
-app.use(express.static("public"));
 
-app.use((req, res, next) => {
-  res.sendFile(path.join("..", "dist", "index.html"));
-});
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+}
+);
+
+app.get('/client', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 
 // start express server on port 5000
 app.listen(process.env.PORT || 5000
