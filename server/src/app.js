59ab943c769fa48c1089678249b6258c943a6eee
vdgaete <vdgaete@gmail.com>
2023-10-02 09:35:25 -0300

    test

---
+++ server/src/app.js
@@ -0,0 +1,45 @@
+/* eslint-disable linebreak-style */
+import {createTable} from './controller/Tarefa.js';
+import express from 'express';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import path from "path";
+import process from "process";
+
+
+const app = express();
+
+
+// Swagger options
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Hello World',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./src/routes*.js'], // files containing annotations as above
+};
+// Initialize swagger-jsdoc -> returns validated swagger spec in json format
+const swaggerSpec = swaggerJSDoc(options);
+
+//client
+app.use(express.static(path.join("..","..", "dist")));
+
+// Json parser
+app.use(express.json());
+
+// Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Check if the database table exists
+createTable();
+
+// Routes
+import router from './routes.js';
+app.use(router);
+
+app.listen(process.env.PORT || 5000, ()=>{
+  console.log(`Server Up And Running At Port ${process.env.PORT}`);
+});
